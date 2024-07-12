bbe871afd0223d90ff8eb99aae3bd7a259cd4d2c
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-12 20:21:14 +0100

    update header

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <header className="py-8 xl:py-12 text-white ">
+      <div className="container mx-auto">
+        {/**logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Haroun<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/**desktopn nav  */}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
