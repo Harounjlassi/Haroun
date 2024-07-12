d64bb6f0f2556c18e4fceeffc953ac3b5fd5f33d
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-12 21:46:38 +0100

    add button to nav

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./ui/NAv";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white ">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/**logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -11,6 +12,14 @@ const Header = () => {
           </h1>
         </Link>
         {/**desktopn nav  */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button> Hire me</Button>
+          </Link>
+        </div>
+        {/**mobile navbar */}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
