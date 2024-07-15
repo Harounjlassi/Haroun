778a6d5fd5d8a23f4e7d9f5bcad5de0a95abf7fd
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-15 15:02:47 +0100

    Add PageTransition

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -18,7 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
