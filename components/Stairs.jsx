11b80d9a2d813b0c330b86e5e60f06da54b25493
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-15 16:07:28 +0100

    update page transtions

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,5 +1,48 @@
+import { motion } from "framer-motion";
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+// calculate the reverse index fot stragged delay
+const reverseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {/* render 6 motion divs  a step of the stairs
+    Each div will have the smae animation defined by the stairsAnimation
+The delay for each div is calculated sinamically based on it's reversed index,
+creating a staggered effect with decreasing delay for each subsequent step.
+    */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default Stairs;
