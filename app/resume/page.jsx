61e5163236462adeabd70443a8c815031b806fc4
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-18 00:43:27 +0100

    update service page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,39 @@
+import { Description } from "@radix-ui/react-dialog";
+import { FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+// about data
+const about = {
+  title: "About me",
+  Description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium minima consequatur, atque distinctio aliquid veritatis dicta exercitationem perferendis voluptas deserunt deleniti quo molestias, laudantium quis odit unde possimus mollitia iusto.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+216) 22 840 408",
+    },
+    {
+      fieldName: "Ex",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+  ],
+};
 const Resume = () => {
   return <div>Resume pages</div>;
 };
