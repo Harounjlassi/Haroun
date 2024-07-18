ae78380f922f6c1655f06c2016279de0b2298fe8
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-18 17:01:20 +0100

    fixv3

--- components/header.jsx
+++ components/header.jsx
@@ -2,24 +2,25 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./ui/Nav";
 import MobileNav from "./ui/MobileNav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white ">
       <div className="container mx-auto flex justify-between items-center">
-        {/**logo */}
+        {/** logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Haroun<span className="text-accent">.</span>
           </h1>
         </Link>
-        {/**desktopn nav  */}
+        {/** desktop nav */}
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button> Hire me</Button>
           </Link>
         </div>
-        {/**mobile navbar */}
+        {/** mobile navbar */}
         <div className="xl:hidden">
           <MobileNav />
         </div>
